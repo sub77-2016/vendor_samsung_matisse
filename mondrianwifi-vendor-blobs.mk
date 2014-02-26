6b4d9ec8ea269e039cf0d6c950eaeff4c988928a
Christopher R. Palmer <crpalmer@gmail.com>
2014-02-26 14:04:45 -0500

    mondrianwifi: Add missing WiFi blobs
    Change-Id: I667ecb513036d0f8456c10e45abe45afc546cbe1

--- mondrianwifi-vendor-blobs.mk
+++ mondrianwifi-vendor-blobs.mk
@@ -197,4 +197,6 @@ PRODUCT_COPY_FILES += \
 	vendor/samsung/mondrianwifi/proprietary/bin/irsc_util:system/bin/irsc_util \
 	vendor/samsung/mondrianwifi/proprietary/etc/firmware/cpp_firmware_v1_1_1.fw:system/etc/firmware/cpp_firmware_v1_1_1.fw \
 	vendor/samsung/mondrianwifi/proprietary/etc/firmware/cpp_firmware_v1_1_6.fw:system/etc/firmware/cpp_firmware_v1_1_6.fw \
-	vendor/samsung/mondrianwifi/proprietary/etc/firmware/cpp_firmware_v1_2_0.fw:system/etc/firmware/cpp_firmware_v1_2_0.fw
+	vendor/samsung/mondrianwifi/proprietary/etc/firmware/cpp_firmware_v1_2_0.fw:system/etc/firmware/cpp_firmware_v1_2_0.fw \
+	vendor/samsung/mondrianwifi/proprietary/etc/firmware/wlan/prima/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
+	vendor/samsung/mondrianwifi/proprietary/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin:system/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin
